42d005d050e80299eb13442b61d2b730898aaea4
Toni Franulic <tonifranulic@Tonis-MacBook-Pro.local>
2020-10-12 21:38:29 +0200

    step 3: remove initial state and branching

--- api.js
+++ api.js
@@ -7,84 +7,41 @@ const pool = new Pool({
   port: 5432
 });
 
-const inMemoryHorrors = [
-  {
-    name: 'The Hills Have Eyes',
-    rating: 7.8
-  },
-  {
-    name: 'The Night of Living Dead',
-    rating: 9.0
-  },
-  {
-    name: 'The Scream',
-    rating: 7.2
-  }
-];
-
 const getAllHorrors = async (request, response) => {
-  if (request.treatment == 'on')
-    pool.query('SELECT * FROM horrors ORDER BY rating ASC', (error, results) => {
-      response.status(200).json(results.rows);
-    });
-  else if (request.treatment == 'off')
-    response.status(200).json(inMemoryHorrors);
-  else
-    response.status(200).json(inMemoryHorrors);
+  pool.query('SELECT * FROM horrors ORDER BY rating ASC', (error, results) => {
+    response.status(200).json(results.rows);
+  });
 };
 
 const getHorrorById = (request, response) => {
   const id = parseInt(request.params.id);
-  if (request.treatment == 'on')
-    pool.query('SELECT * FROM horrors WHERE id = $1', [id], (error, results) => {
-      response.status(200).json(results.rows);
-    });
-  else if (request.treatment == 'off')
-    response.status(200).json(inMemoryHorrors[0]);
-  else
-    response.status(200).json(inMemoryHorrors[0]);
+  pool.query('SELECT * FROM horrors WHERE id = $1', [id], (error, results) => {
+    response.status(200).json(results.rows);
+  });
 };
 
 const addHorror = async (request, response) => {
   const { name, rating } = request.body;
-  console.log("name, rating", name, rating)
-  if (request.treatment == 'on')
-    pool.query('INSERT INTO horrors (name, rating) VALUES ($1, $2)', [name, rating], (error, results) => {
-      response.status(201).send(`Horror added successfuly.`);
-    });
-  else {
-    inMemoryHorrors.push({ name, rating });
+  pool.query('INSERT INTO horrors (name, rating) VALUES ($1, $2)', [name, rating], (error, results) => {
     response.status(201).send(`Horror added successfuly.`);
-  }
+  });
 };
 
 const updateHorror = (request, response) => {
   const id = parseInt(request.params.id);
   const { name, rating } = request.body;
-
-  if (request.treatment == 'on')
-    pool.query(
-      'UPDATE horrors SET name = $1, rating = $2 WHERE id = $3', [name, rating, id], (error, results) => {
-        response.status(200).send(`Horror modified with ID: ${id}`);
-      }
-    );
-  else {
-    inMemoryHorrors[0] = { name, rating };
-    response.status(200).send(`Horror modified with ID: ${id}`);
-  }
+  pool.query(
+    'UPDATE horrors SET name = $1, rating = $2 WHERE id = $3', [name, rating, id], (error, results) => {
+      response.status(200).send(`Horror with id ${id} modified.`);
+    }
+  );
 };
 
 const deleteHorror = (request, response) => {
   const id = parseInt(request.params.id);
-
-  if (request.treatment == 'on')
-    pool.query('DELETE FROM horrors WHERE id = $1', [id], (error, results) => {
-      response.status(200).send(`Horror deleted with ID: ${id}`);
-    });
-  else {
-    inMemoryHorrors.shift();
-    response.status(200).send(`Horror deleted with ID: ${id}`);
-  }
+  pool.query('DELETE FROM horrors WHERE id = $1', [id], (error, results) => {
+    response.status(200).send(`Horror with id ${id} deleted.`);
+  });
 };
 
 module.exports = {
